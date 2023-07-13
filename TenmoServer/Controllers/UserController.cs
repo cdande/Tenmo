623ae5430e3dd1d0dcfb1de4d0de284ddb8a5ea6
cdande <133425619+cdande@users.noreply.github.com>
2023-07-13 16:14:25 -0400

    Add files via upload

---
+++ TenmoServer/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using TenmoServer.DAO;
+using TenmoServer.Exceptions;
+using TenmoServer.Models;
+using TenmoServer.Security;
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class UserController : Controller
+    {
+
+        private readonly IUserDao userDao;
+        public UserController(IUserDao userDao)
+        {
+            this.userDao = userDao;
+        }
+        [Authorize]
+        [HttpGet()]
+        public ActionResult<IList<User>> GetUsers()
+        {
+            IList<User> users = new List<User>();
+            users = userDao.GetUsers();
+            if (users != null)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [Authorize]
+        [HttpGet("account/{id}")]
+        public ActionResult<User> GetUserByAccountId(int id)
+        {
+            User user = null;
+            user = userDao.GetUserByAccountId(id);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+    }
+}
